c5401321a539c73c3354e76f094615a1f2ec4af9
Nilanjan Dey <deynilanjan2004@gmail.com>
2024-08-25 11:43:03 +0530

    Add files via upload
    Problem Type: Easy        Topics: Basic       Language: Java         Date: 25-08-2024      Repetition: False

---
+++ Coding/code2.java
@@ -0,0 +1,9 @@
+class code2{
+    public void restart_coding_journey(){
+        System.out.println("Hello world");
+    }
+    public static void main(String args[]){
+        code2 cd = new code2();
+        cd.restart_coding_journey();
+    }
+}
