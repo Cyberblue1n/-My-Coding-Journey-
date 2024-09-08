5c7a90657cc00310fa9f50e86436fd527377765f
Nilanjan Dey <deynilanjan2004@gmail.com>
2024-09-08 19:56:03 +0530

    Day 2/ 08-09-2024
    Day: 2      Problem Type: Easy      Topics: String      Language: Python   Date: 08-09-2024   Platform: geeksforgeeks

---
+++ Coding/code5.py
@@ -0,0 +1,21 @@
+'''Python program to check whether the string is Symmetrical or Palindrome
+Input: khokho
+Output: 
+The entered string is symmetrical
+The entered string is not palindrome
+
+Input:amaama
+Output:
+The entered string is symmetrical
+The entered string is palindrome'''
+
+def ispalindrome(s):
+    if s==s[::-1]:
+        return 1
+if __name__ == "__main__":
+    s = input("Enter word: ")
+    a = ispalindrome(s)
+    if a==1:
+        print(f"{s} is palindrome")
+    else:
+        print(f"{s} is not palindrome")
