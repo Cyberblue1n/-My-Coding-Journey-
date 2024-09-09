6a36f33336794566295c348690dd3c6c183154d5
Nilanjan Dey <deynilanjan2004@gmail.com>
2024-09-09 21:33:18 +0530

    code5.py

--- Coding/code5.py
+++ Coding/code5.py
@@ -1,12 +1,10 @@
-'''Python program to check whether the string is Symmetrical or Palindrome
+'''Python program to check whether the string is Palindrome
 Input: khokho
 Output: 
-The entered string is symmetrical
 The entered string is not palindrome
 
 Input:amaama
 Output:
-The entered string is symmetrical
 The entered string is palindrome'''
 
 def ispalindrome(s):
